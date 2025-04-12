eb241f9ff72600cb2fa3ccf8fb836d6c83b4d37e
alessandroschiano98 <schianoski98@gmail.com>
2025-04-12 12:11:16 +0200

    esercizio

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,33 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        // - NOME
+        System.out.println("Inserisci un nome");
+        String nome = input.nextLine();
+        System.out.println("Il nome che  hai appena inserito e': " + nome);
+
+        // - COGNOME
+        System.out.println("Inserisci un cognome");
+        String cognome = input.nextLine();
+        System.out.println("Il cognome che  hai appena inserito e': " + cognome);
+
+        // - COLORE
+        System.out.println("Inserisci il tuo colore preferito");
+        String colorePreferito = input.nextLine();
+        System.out.println("Il tuo colore preferito è: " + colorePreferito);
+
+
+        String passwordGenerator = input.nextLine();
+        System.out.printIn("La password che hai generato è:" + nome + cognome + colorePreferito);
+       
+
+
         
+        input.close();
     }
 }
